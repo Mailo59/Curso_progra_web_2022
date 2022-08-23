836ab07c799716f9ee7b409661aaa5e642105cb6
Mailo59 <emiliomorales@ubuntu.com>
2022-08-23 16:59:00 -0700

    Se añadieron 2 paginas mas

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let hideText_btn = document.getElementById('hideText_btn');
+let hidetext = document.getElementById('hidetext');
+
+
+hideText_btn.addEventListener('click', toggleText);
+
+function toggleText(){
+    hidetext.classList.toggle('show');
+
+    if(hidetext.classList.contains('show')){
+        hideText_btn.innerHTML = 'Leer Menos'
+
+    }else{
+        hideText_btn.innerHTML = 'Leer Mas'
+    }
+}
