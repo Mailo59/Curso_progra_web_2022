a7458e4259a50ed39c715d80a8b3ab034bd2e3e9
Mailo59 <emiliomorales@ubuntu.com>
2022-09-03 16:28:21 -0700

    Clase sabado sobre JS

---
+++ reproductor/js/aplication.js
@@ -0,0 +1,27 @@
+function iniciar(){
+    maximo= 600;
+    medio = document.getElementById('medio');
+    reproducir = document.getElementById('reproducir');
+    barra = document.getElementById('barra');
+    progreso = document.getElementById('progreso');
+    
+
+    reproducir.addEventListener('click', presionar, false);
+    barra.addEventListener('click', mover, false);
+
+}
+
+function presionar(){
+    if(!medio.paused && !medio.ended){
+        medio.paused();
+        reproducir.innerHTML = 'Reproducir';
+        window.clearInterval(bucle);
+
+
+    }else{
+        medio.play();
+        reproducir.innerHTML= 'Pausar';
+        bucle =setInterval(estado, 100);
+    }
+    
+}
